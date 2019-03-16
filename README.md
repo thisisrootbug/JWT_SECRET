76c4e411e994362a885d2f2b9b98ef346a1cf74e
root <root@localhost.localdomain>
2019-03-17 00:24:32 +0200

    adding algorithm option

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # JWT_SECRET
-a simple python script to brute force JSON WEB TOCKEN (JWT) secret using a dictionary 
+	a simple python script to brute force JSON WEB TOCKEN (JWT) secret using a dictionary 
+
+# usage 
+	./jwt_secret_brute.py -t <JWT> -a <hashing algorithm> -w <word list>	
--- jwt_secret_brute.py
+++ jwt_secret_brute.py
@@ -12,10 +12,12 @@ RESET="\033[0m"
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', help='JWT Tocken', dest='tocken')
+    parser.add_argument('-a', help='hashing algorithm default HS256', dest='algo')
     parser.add_argument('-w', help='word list default STDIN', dest='word_list')
     args = parser.parse_args()
     counter = 0
     token = args.tocken
+    algo = args.algo or 'HS256'
     if(args.word_list):
         word_list = open(args.word_list,"r")
     else:
@@ -25,11 +27,12 @@ if __name__ == "__main__":
     for secret in word_list.readlines():
         counter += 1
         try:
-            decoded = jwt.decode(token,key=secret.strip(),algorithm=sys.argv[2])
+            decoded = jwt.decode(token,key=secret.strip(),algorithm=algo)
             sys.stdout.write(GREEN+"[+] Secret Found  "+secret+RESET+"\n"+str(decoded))
             sys.stdout.flush()
             break
         except Exception as ops:
+            # in case of wrong secret show status
             if(counter % 100 == 0):
                 sys.stdout.write(str(counter)+"   "+secret.strip()+"        ")
                 sys.stdout.flush()
